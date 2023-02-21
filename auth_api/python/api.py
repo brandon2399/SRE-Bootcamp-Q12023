f9dbea77c70ec23af8fa8407a311a95137f62fe1
Brandon Concha <brandon2399@hotmail.com>
2023-02-21 15:48:29 -0500

    Define class token

--- auth_api/python/api.py
+++ auth_api/python/api.py
@@ -1,11 +1,32 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, jsonify, request
+import hashlib
+import hmac
+import jwt
 from methods import Token, Restricted
 
 app = Flask(__name__)
-login = Token()
+login = Token("my2w7wjd7yXF64FIADfJxNs1oupTGAuW")
 protected = Restricted()
+
+# Define the users database
+
+users = {
+    'admin': {
+        'password': 'encrypted-password',
+        'salt': 'F^S%QljSfV',
+        'role': 'admin'
+    },
+    'noadmin': {
+        'password': 'encrypted-password',
+        'salt': 'KjvFUC#K*i',
+        'role': 'editor'
+    },
+    'bob': {
+        'password': 'encrypted-password',
+        'salt': 'F^S%QljSfV',
+        'role': 'viewer'
+    }
+}
 
 
 # Just a health check
@@ -25,16 +46,14 @@ def url_health():
 def url_login():
     username = request.form['username']
     password = request.form['password']
-    res = {
-        "data": login.generate_token(username, password)
-    }
-    return jsonify(res)
+    res, status_code = login.generate_token(username, password)
+    return jsonify(res), status_code
 
 
-# # e.g. http://127.0.0.1:8000/protected
+# e.g. http://127.0.0.1:8000/protected
 @app.route("/protected")
 def url_protected():
-    auth_token = request.headers.get('Authorization')
+    auth_token = request.headers.get('Authorization').split(" ")[1]
     res = {
         "data": protected.access_data(auth_token)
     }
--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,11 +1,24 @@
-# These functions need to be implemented
 class Token:
-
+    def __init__(self):
+        self.users = users
+        
     def generate_token(self, username, password):
-        return 'test'
+        if username not in self.users:
+            return {"error": "Invalid username."}, 403
+        
+        # Verify the password
+        user = self.users[username]
+        hashed_password = hashlib.sha512((user['salt'] + password).encode()).hexdigest()
+        if user['password'] != hashed_password:
+            return {"error": "Invalid password."}, 403
+        
+        # Generate the JWT Token
+        payload = {
+            'sub': username,
+            'name': username,
+            'role': user['role']
+        }
+        token = jwt.encode(payload, key=SECRET_KEY, algorithm='HS256')
+        return {"data": token}, 200
 
 
-class Restricted:
-
-    def access_data(self, authorization):
-        return 'test'
